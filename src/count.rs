d5b10afe6fc1675413b6dcd7a77064ada4ee4ceb
alsmith151 <alastair.smith@ndcls.ox.ac.uk>
2024-08-01 15:08:44 +0100

    Refactor code to use println! instead of print! for displaying the number of intervals

--- src/count.rs
+++ src/count.rs
@@ -147,7 +147,7 @@ impl BamPileup {
                     })
                     .collect::<Vec<Iv>>();
 
-                print!("Number of intervals {}", intervals.len());
+                println!("Number of intervals {}", intervals.len());
 
                 // Create a lapper from the intervals
                 let lapper = Lapper::new(intervals);
