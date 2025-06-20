1008e1b96be8fdc5458cdf84cc7d92391348bf19
Alastair Smith <49727900+alsmith151@users.noreply.github.com>
2025-06-20 16:20:39 +0100

    Chore allow access to external crates (#27)
    * refactor: reorder module declarations and update imports for clarity
    
    * chore(Cargo.toml): bump version to 0.3.0
    
    * docs: update installation instructions in README.md for clarity and simplicity

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,18 @@
-mod utils;
-mod filter;
-mod intervals;
-mod spikein;
-// mod count;
-mod pileup;
-mod normalization;
+pub mod utils;
+pub mod filter;
+pub mod intervals;
+pub mod spikein;
+pub mod pileup;
+pub mod normalization;
+pub mod split;
 
 
+pub use utils::{
+    BamStats,
+    CellBarcodes,
+    CellBarcodesMulti,
+    Strand,
+};
 
 
 
