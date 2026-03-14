17ca0adbff55fc6449bd3feadc37726a518af687
alsmith151 <alsmith151@users.noreply.github.com>
2026-03-14 18:52:33 +0000

    deploy: f4a7482684fbcbe0be949c59b385e9becbcf747a

--- trait.impl/core/cmp/trait.Eq.js
+++ trait.impl/core/cmp/trait.Eq.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["bamnado",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"bamnado/bam_utils/enum.Strand.html\" title=\"enum bamnado::bam_utils::Strand\">Strand</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"bamnado/genomic_intervals/struct.Shift.html\" title=\"struct bamnado::genomic_intervals::Shift\">Shift</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.92.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"bamnado/genomic_intervals/struct.Truncate.html\" title=\"struct bamnado::genomic_intervals::Truncate\">Truncate</a>"]]]]);
+    const implementors = Object.fromEntries([["bamnado",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"enum\" href=\"bamnado/bam_utils/enum.Strand.html\" title=\"enum bamnado::bam_utils::Strand\">Strand</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"bamnado/genomic_intervals/struct.Shift.html\" title=\"struct bamnado::genomic_intervals::Shift\">Shift</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.0/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"bamnado/genomic_intervals/struct.Truncate.html\" title=\"struct bamnado::genomic_intervals::Truncate\">Truncate</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[807]}
+//{"start":59,"fragment_lengths":[813]}
--- trait.impl/serde_core/ser/trait.Serialize.js
+++ trait.impl/serde_core/ser/trait.Serialize.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["bamnado",[["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"bamnado/spike_in_analysis/struct.SplitStats.html\" title=\"struct bamnado::spike_in_analysis::SplitStats\">SplitStats</a>"]]]]);
+    const implementors = Object.fromEntries([["bamnado",[["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"bamnado/spike_in_analysis/struct.SplitStats.html\" title=\"struct bamnado::spike_in_analysis::SplitStats\">SplitStats</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[331]}
+//{"start":59,"fragment_lengths":[333]}
